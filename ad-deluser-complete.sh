084fb52130d00bde10884d2d11adabe652610546
benbelo <benjamin.belot@proton.me>
2025-09-08 16:25:04 +0200

    commentaires

--- ad-deluser-complete.sh
+++ ad-deluser-complete.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 . /usr/local/sbin/ad-conffiles
 kinit script@LAN.ENTREPRISE.FR <<< $etpaf >/dev/null
 
@@ -6,7 +8,7 @@ kinit script@LAN.ENTREPRISE.FR <<< $etpaf >/dev/null
 LIST=$@
 [ $# -eq 1 ] && [ -f $1 ] && LIST=$(cat $1 | tr -d '\r' | grep -v 'login.Individu')
 
-# Too late !
+# Trop tard...
 
 GLOBALERROR=0
 for user in $LIST ; do
@@ -14,7 +16,7 @@ for user in $LIST ; do
     samba-tool user show $user >/dev/null
     [ $? -ne 0 ] && exit 1
 
-    # Creating gaccount variable for further use
+    # Variable de compte pour plus tard
     gaccount=$(search-ad.sh -l $user | awk '{print $3}')
 
     ERROR=0
@@ -47,13 +49,13 @@ for user in $LIST ; do
         echo "$user deleted in old LDAP"
     fi
 
-    ## Asking for deletion
+    ## Demande de suppression
     read -p "Are you sure you want to delete this Google account ? [yn]" answer
         if [[ $answer = y ]] ; then
                gmail-deluser-mailEntreprise.sh $gaccount;
         fi
 
-    # Log and mail
+    # Log & mail
     logs "Del user $user (AD+LDAP+Homedir) by $(get_ident)"
     echo "$(basename $0) --- $user" |mail -s "$(basename $0) --- $user" compte-info@entreprise.fr
 
